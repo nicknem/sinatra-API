3697e1721ce7ea3361574b2c58866c2b1bbc32ee
nicknem <antonin.archer@hec.edu>
2016-07-13 18:08:45 +0200

    Add endpoint for index, show.

---
+++ book.rb
@@ -0,0 +1,19 @@
+class Book
+  include Mongoid::Document
+
+  field :title, type: String
+  field :author, type: String
+  field :isbn, type: String
+
+  validates :title, presence: true
+  validates :author, presence: true
+  validates :isbn, presence: true
+
+  index({ title: 'text' })
+  index({ isbn: 1 }, { unique: true, name: "isbn_index" })
+
+  # Did not understand scopes
+  scope :title, -> (title) { where(title: /^#{title}/) }
+  scope :isbn, -> (isbn) { where(isbn: isbn) }
+  scope :author, -> (author) { where(author: author) }
+end
--- server.rb
+++ server.rb
@@ -1,27 +1,41 @@
 # server.rb
 require 'sinatra'
+require 'sinatra/namespace'
 require 'mongoid'
+require_relative 'book' # Require the Book model
+require_relative 'book_serializer'
 
 # DB Setup
 Mongoid.load! "mongoid.config"
 
-# Models
-class Book
-  include Mongoid::Document
 
-  field :title, type: String
-  field :author, type: String
-  field :isbn, type: String
-
-  validates :title, presence: true
-  validates :author, presence: true
-  validates :isbn, presence: true
-
-  index({ title: 'text' })
-  index({ isbn: 1 }, { unique: true, name: "isbn_index" })
-end
 
 # Endpoints
 get '/' do
   'Welcome to BookList!'
+end
+
+namespace '/api/v1' do
+
+  before do
+    content_type 'application/json'
+  end
+
+  # Index
+  get '/books' do
+    books = Book.all
+    [:title, :isbn, :author].each do |filter|
+      books = books.send(filter, params[filter]) if params[filter]
+    end
+
+    books.map { |book| BookSerializer.new(book) }.to_json
+  end
+
+  # Show
+  get '/books/:id' do |id|
+    book = Book.where(id: id).first
+    halt(404, { message: 'Book Not Found'}.to_json) unless book
+    BookSerializer.new(book).to_json
+  end
+
 end
